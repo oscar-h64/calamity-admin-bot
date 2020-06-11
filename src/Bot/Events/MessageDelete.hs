1da106f20e6320b857d29ee5018035b8c23659ea
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 20:59:50 +0100

    Implement onMessageDelete

--- src/Bot/Events/MessageDelete.hs
+++ src/Bot/Events/MessageDelete.hs
@@ -1,6 +1,22 @@
 module Bot.Events.MessageDelete where
 
+import Data.Default
+import Data.Colour.Names
+
 import Bot.Import
 
 onMessageDelete :: BotC r => Message -> Sem r ()
-onMessageDelete = undefined
+onMessageDelete m = do
+    let text     = m ^. #content
+        author   = m ^. #author
+        channel  = m ^. #channelID
+        origtime = fromStrict $ showt $ m ^. #timestamp
+        embed = def & #title ?~ "Message Deleted" 
+                    & #color ?~ red
+                    & #fields .~ [
+                        EmbedField "Author" (mention author) True,
+                        EmbedField "Sent" origtime True,
+                        EmbedField "Old Text" text True,
+                        EmbedField "Channel" (mention channel) True
+                    ]
+    void $ tell @Embed logChannel embed
