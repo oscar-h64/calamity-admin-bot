979826a85b998724e507c9f8fd21ff5fa5ea987d
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 10:36:35 +0100

    Add basic ping command

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+
 module Main where
 
-import Lib
+import           Calamity
+import           Calamity.Cache.InMemory
+import           Calamity.Commands
+import qualified Calamity.Commands.Context                  as CommandContext
+import           Calamity.Metrics.Noop
+
+import           Control.Monad
+
+import           Data.Text                                  ( Text )
+import qualified Data.Text.Lazy                             as L
+
+import qualified DiPolysemy                                 as DiP
+
+import qualified Polysemy                                   as P
+
+import           Prelude                                    hiding ( error )
+
+import           TextShow
+
+info, debug :: BotC r => Text -> P.Sem r ()
+info = DiP.info
+debug = DiP.info
+
+tellt :: (BotC r, Tellable t) => t -> L.Text -> P.Sem r (Either RestError Message)
+tellt t m = tell t $ L.toStrict m
 
 main :: IO ()
-main = someFunc
+main = do
+  let token = ""
+  void . P.runFinal . P.embedToFinal . runCacheInMemory . runMetricsNoop . useConstantPrefix "!"
+    $ runBotIO (BotToken token) $ do
+    addCommands $ do
+      helpCommand
+      command @'[] "ping" $ \ctx ->
+        void $ tellt ctx "pong"
+    react @('CustomEvt "command-error" (CommandContext.Context, CommandError)) $ \(ctx, e) -> do
+      info $ "Command failed with reason: " <> showt e
+      case e of
+        ParseError n r -> void . tellt ctx $ "Failed to parse parameter: `" <> L.fromStrict n <> "`, with reason: ```\n" <> r <> "```"
