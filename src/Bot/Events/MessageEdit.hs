58e4c3f9965eaa68a91c05cf4978fb7d22188c81
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 16:56:29 +0100

    Add update message

--- src/Bot/Events/MessageEdit.hs
+++ src/Bot/Events/MessageEdit.hs
@@ -4,5 +4,15 @@ import Calamity.Types.Model.Channel.Message
 
 import Bot.Import
 
+import TextShow
+
+import qualified Data.Text.Lazy as L
+
 onMessageEdit :: BotC r => Message -> Message -> Sem r ()
-onMessageEdit = undefined
+onMessageEdit m1 m2 = do
+    let orig = m1 ^. #content
+        new  = m2 ^. #content
+        auth = m2 ^. #author
+        chan = m2 ^. #channelID
+        time = m2 ^. #editedTimestamp
+    void $ tellt m2 $ L.fromStrict $ showt auth <> " updated their message in " <> showt chan <> " from " <> L.toStrict orig <> " to " <> L.toStrict new <> " at " <> showt time
