5a6879c603fb8e7226a6720f58c63f44805d6bb1
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 20:15:42 +0100

    Rearrange fields

--- src/Bot/Events/MessageEdit.hs
+++ src/Bot/Events/MessageEdit.hs
@@ -12,21 +12,19 @@ onMessageEdit :: BotC r => Message -> Message -> Sem r ()
 onMessageEdit m1 m2 = do
     let origtext = m1 ^. #content
         newtext  = m2 ^. #content
-        author = m2 ^. #author
-        -- getAvatar u = u ^. #avatar
-        channel = m2 ^. #channelID
+        author   = m2 ^. #author
+        channel  = m2 ^. #channelID
         origtime = fromStrict $ showt $ m2 ^. #timestamp
         edittime = fromStrict $ fromMaybe "" $ showt <$> m2 ^. #editedTimestamp
-    -- fullAuth <- upgrade author
-    let msg :: Embed
+        msg :: Embed
         msg = def & #title ?~ "Message Edited" 
-                --   & #author ?~ EmbedAuthor (displayUser <$> fullAuth) Nothing (getAvatar <$> fullAuth) Nothing
                   & #fields .~ [
                       EmbedField "Author" (mention author) True,
-                      EmbedField "Channel" (mention channel) True,
                       EmbedField "Sent" origtime True,
-                      EmbedField "Edited" edittime True,
                       EmbedField "Old Text" origtext True,
+
+                      EmbedField "Channel" (mention channel) True,
+                      EmbedField "Edited" edittime True,                      
                       EmbedField "New Text" newtext True
                   ]
     void $ tell logChannel msg
