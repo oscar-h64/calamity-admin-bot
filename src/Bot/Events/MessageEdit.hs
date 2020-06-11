ea1185445741269264b8e6c8d4e19371fadf4cca
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 17:05:14 +0100

    Add mentions to edit text

--- src/Bot/Events/MessageEdit.hs
+++ src/Bot/Events/MessageEdit.hs
@@ -4,15 +4,11 @@ import Calamity.Types.Model.Channel.Message
 
 import Bot.Import
 
-import TextShow
-
-import qualified Data.Text.Lazy as L
-
 onMessageEdit :: BotC r => Message -> Message -> Sem r ()
 onMessageEdit m1 m2 = do
     let orig = m1 ^. #content
         new  = m2 ^. #content
         auth = m2 ^. #author
         chan = m2 ^. #channelID
-        time = m2 ^. #editedTimestamp
-    void $ tellt m2 $ L.fromStrict $ showt auth <> " updated their message in " <> showt chan <> " from " <> L.toStrict orig <> " to " <> L.toStrict new <> " at " <> showt time
+        time = fromStrict $ fromMaybe "" $ showt <$> m2 ^. #editedTimestamp
+    void $ tellt m2 $ mention auth <> " updated their message in " <> mention chan <> " from " <> orig <> " to " <> new <> " at " <> time
