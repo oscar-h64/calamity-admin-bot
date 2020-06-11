f41dbcea1cb929ef7d8810bd7c0bb40e7edd953f
Oscar Harris <ozj.harris@gmail.com>
2020-06-11 19:01:36 +0100

    Change log to use embed

--- src/Bot/Events/MessageEdit.hs
+++ src/Bot/Events/MessageEdit.hs
@@ -1,6 +1,10 @@
 module Bot.Events.MessageEdit where
 
 import Calamity.Types.Model.Channel.Message
+import Calamity.Types.Model.Channel.Embed
+import Calamity.Types.Tellable
+
+import Data.Default
 
 import Bot.Import
 
@@ -9,8 +13,20 @@ onMessageEdit m1 m2 = do
     let origtext = m1 ^. #content
         newtext  = m2 ^. #content
         author = m2 ^. #author
+        -- getAvatar u = u ^. #avatar
         channel = m2 ^. #channelID
         origtime = fromStrict $ showt $ m2 ^. #timestamp
         edittime = fromStrict $ fromMaybe "" $ showt <$> m2 ^. #editedTimestamp
-    
-    void $ tellt logChannel $ mention author <> " updated their message in " <> mention channel <> " from " <> origtext <> " to " <> newtext <> " at " <> edittime
+    -- fullAuth <- upgrade author
+    let msg :: Embed
+        msg = def & #title ?~ "Message Edited" 
+                --   & #author ?~ EmbedAuthor (displayUser <$> fullAuth) Nothing (getAvatar <$> fullAuth) Nothing
+                  & #fields .~ [
+                      EmbedField "Author" (mention author) True,
+                      EmbedField "Channel" (mention channel) True,
+                      EmbedField "Sent" origtime True,
+                      EmbedField "Edited" edittime True,
+                      EmbedField "Old Text" origtext True,
+                      EmbedField "New Text" newtext True
+                  ]
+    void $ tell logChannel msg
