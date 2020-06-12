9e16cdb0b3d5fa8825db9be65d9729b5a4947ce6
Oscar H <34679874+oscar-h64@users.noreply.github.com>
2020-06-12 14:42:19 +0100

    Add name to license file and add license notice to every hs file (#16)
    * Add name to license
    
    * Fix typo in stack.yaml
    
    * Add license info to every hs file

--- src/Bot/Events/MessageEdit.hs
+++ src/Bot/Events/MessageEdit.hs
@@ -1,3 +1,11 @@
+--------------------------------------------------------------------------------
+-- Discord Test Bot                                                           --
+--------------------------------------------------------------------------------
+-- This source code is licensed under the BSD 3-Clause licence found in the   --
+-- LICENSE file in the root directory of this source tree.                    --
+--                                                                            --
+-- Copyright 2020 Oscar Harris (oscar@oscar-h.com)                            --
+--------------------------------------------------------------------------------
 module Bot.Events.MessageEdit where
 
 import Data.Default
