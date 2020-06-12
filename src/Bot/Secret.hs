eb78be706a65a4374cb5c84ff84d4f347f654214
Oscar H <34679874+oscar-h64@users.noreply.github.com>
2020-06-12 15:01:39 +0100

    Change BSD 3-Clause to BSD3 (#18)

--- src/Bot/Secret.hs
+++ src/Bot/Secret.hs
@@ -1,8 +1,8 @@
 --------------------------------------------------------------------------------
 -- Discord Test Bot                                                           --
 --------------------------------------------------------------------------------
--- This source code is licensed under the BSD 3-Clause licence found in the   --
--- LICENSE file in the root directory of this source tree.                    --
+-- This source code is licensed under the BSD3 licence found in the LICENSE   --
+-- file in the root directory of this source tree.                            --
 --                                                                            --
 -- Copyright 2020 Oscar Harris (oscar@oscar-h.com)                            --
 --------------------------------------------------------------------------------
