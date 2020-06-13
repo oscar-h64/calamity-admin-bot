e2a4efc136cec76faa84b787ef92618614724afc
Oscar Harris <ozj.harris@gmail.com>
2020-06-13 19:38:00 +0100

    Add ban and unban commands (#22)
    * Add basic command structure
    
    * Ban implemented (hopefully)
    
    * Add help messages
    
    * Fix reason for ban
    
    * Finalise ban function
    
    * Change Banner field to Admin
    
    * Add admin check
    
    * Generalise admin actions
    
    * Make import specific
    
    * Add unban command

--- app/Main.hs
+++ app/Main.hs
@@ -34,11 +34,21 @@ main = void . P.runFinal . P.embedToFinal . runCacheInMemory . runMetricsNoop . 
                 helpCommand
 
                 -- Ping Command
-                command @'[] "ping" ping
+                help (const "Replies with 'pong'") $
+                    command @'[] "ping" ping
 
                 -- Invite command
-                command @'[] "invite" invite
-              
+                help (const "Returns the invite link to the server") $
+                    command @'[] "invite" invite
+
+                -- User Ban
+                adminCheck $ help (const "Bans the given user for the given reason") $
+                    command @'[User, [Text]] "ban" ban
+                
+                -- User Unban
+                adminCheck $ help (const "Unbans the given user for the given reason") $
+                    command @'[User, [Text]] "unban" unban
+
             -- Event Handlers:
             -- Message Edit:
             react @'MessageUpdateEvt $ uncurry onMessageEdit
@@ -48,6 +58,9 @@ main = void . P.runFinal . P.embedToFinal . runCacheInMemory . runMetricsNoop . 
 
             -- Command Error Event
             react @('CustomEvt "command-error" (CommandContext, CommandError)) $ \(ctx, e) -> do
-              info $ "Command failed with reason: " <> showt e
-              case e of
-                ParseError n r -> void . tellt ctx $ "Failed to parse parameter: `" <> L.fromStrict n <> "`, with reason: ```\n" <> r <> "```"
+                info $ "Command failed with reason: " <> showt e
+                case e of
+                    ParseError n r -> void . tellt ctx $ 
+                        "Failed to parse parameter: `" <> L.fromStrict n <> "`, with reason: ```\n" <> r <> "```"
+                    CheckError n r -> void . tellt ctx $ 
+                        "The following check failed: " <> codeline (L.fromStrict n) <> ", with reason: " <> codeblock' Nothing r
---
+++ src/Bot/Commands/Admin.hs
@@ -0,0 +1,63 @@
+--------------------------------------------------------------------------------
+-- Discord Test Bot                                                           --
+--------------------------------------------------------------------------------
+-- This source code is licensed under the BSD3 licence found in the LICENSE   --
+-- file in the root directory of this source tree.                            --
+--                                                                            --
+-- Copyright 2020 Oscar Harris (oscar@oscar-h.com)                            --
+--------------------------------------------------------------------------------
+module Bot.Commands.Admin (
+    adminCheck,
+    AdminLoggable(..),
+    doAdminAction
+) where
+
+import           Calamity.Commands.Dsl ( DSLState, requiresPure )
+
+import           Data.Colour (Colour)
+import           Data.Default
+import qualified Data.Text.Lazy as L ( Text )
+import           Data.Time.Clock ( getCurrentTime )
+import qualified Data.Vector.Unboxed as V ( elem )
+
+import qualified Polysemy as P
+
+import           Bot.Import
+
+check :: CommandContext -> Maybe L.Text
+check ctx = do
+    member <- ctx ^. #member
+    if Snowflake 720279447776788540 `V.elem` member ^. #roles then 
+        Nothing 
+    else 
+        Just "You must be an administator to do that"
+
+adminCheck :: Sem (DSLState r) a -> Sem (DSLState r) a
+adminCheck = requiresPure [("adminCheck", check)]
+
+class AdminLoggable a where
+    colour :: AdminLoggable a => Colour Double
+    word :: AdminLoggable a => Text
+
+type ToInvoke = Guild -> GuildRequest ()
+
+doAdminAction :: forall action r 
+               . (AdminLoggable action, BotC r) 
+              => CommandContext 
+              -> User 
+              -> ToInvoke 
+              -> [EmbedField] 
+              -> Sem r ()
+doAdminAction ctx u toInvoke fields = case ctx ^. #guild of
+    Nothing -> void $ tellt ctx "Administrator actions must be performed in a guild"
+    Just g -> do
+        invoke $ toInvoke g
+        tellt ctx $ fromStrict (word @action) <> " " <> mention u
+        time <- P.embed getCurrentTime
+        let embed = def & #title ?~ fromStrict ("User " <> word @action) 
+                        & #color ?~ colour @action
+                        & #fields .~ EmbedField "User" (mention u) True
+                                    : EmbedField "Admin" (mention $ ctx ^. #user) True
+                                    : EmbedField "Time" (showtl time) True
+                                    : fields
+        void $ tell @Embed logChannel embed
