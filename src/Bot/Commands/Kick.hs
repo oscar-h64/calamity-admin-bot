6cab34c54b74991f898a3d5baf41256787854f6a
Oscar Harris <ozj.harris@gmail.com>
2020-06-15 19:08:00 +0100

    Add basic Kick structure

--- src/Bot/Commands/Kick.hs
+++ src/Bot/Commands/Kick.hs
@@ -6,20 +6,18 @@
 --                                                                            --
 -- Copyright 2020 Oscar Harris (oscar@oscar-h.com)                            --
 --------------------------------------------------------------------------------
-module Bot.Commands (
-    ping,
-    invite,
-    mute,
-    unmute,
-    ban,
-    unban,
-    kick,
-    adminCheck
-) where
+module Bot.Commands.Kick where
 
-import Bot.Commands.Ping
-import Bot.Commands.Invite
-import Bot.Commands.Mute
-import Bot.Commands.Ban
-import Bot.Commands.Kick
-import Bot.Commands.Admin (adminCheck)
+import Data.Colour.Names ( darkmagenta )
+
+import Bot.Import
+import Bot.Commands.Admin
+
+data Kick
+
+instance AdminLoggable Kick where
+    colour = darkmagenta
+    word = "Kicked"
+
+kick :: (BotC r) => CommandContext -> Snowflake User -> [Text] -> Sem r ()
+kick = undefined
