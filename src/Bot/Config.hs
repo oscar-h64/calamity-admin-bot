dc04b2dae1ee2760eedf1b57aa9049b503d53ddd
Oscar Harris <oscar@oscar-h.com>
2020-06-19 18:30:21 +0100

    Update title in copyright notice

--- src/Bot/Config.hs
+++ src/Bot/Config.hs
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- Discord Test Bot                                                           --
+-- Calamity Admin Bot                                                         --
 --------------------------------------------------------------------------------
 -- This source code is licensed under the BSD3 licence found in the LICENSE   --
 -- file in the root directory of this source tree.                            --
